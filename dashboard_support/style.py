74868830032c9e42da8a55e791a1f60cd8b4ecb6
Nyarlathoteppppp <xubowen.ton618@gmail.com>
2026-08-11 17:27:30 +1000

    Set dashboard pink transparency to fifty percent

--- dashboard_support/style.py
+++ dashboard_support/style.py
@@ -5,7 +5,7 @@ QWidget {
     font-family: 'Helvetica Neue', Arial, sans-serif;
 }
 QWidget#DashboardRoot {
-    background-color: rgba(255, 184, 211, 72);
+    background-color: rgba(255, 184, 211, 128);
 }
 QWidget#DashboardRoot[fullscreenFallback="true"] {
     background-color: rgba(28, 30, 39, 252);
